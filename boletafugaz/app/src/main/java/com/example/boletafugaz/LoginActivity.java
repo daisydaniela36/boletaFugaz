1364f9235181490c5d543638aaade20f7dfccc38
daisydaniela36 <33319268+daisydaniela36@users.noreply.github.com>
2021-04-16 18:22:06 -0400

    conectar boton del login
    se conecto el boton del login ir a crear cuenta con la actividad signup para que se redirija hay

--- boletafugaz/app/src/main/java/com/example/boletafugaz/LoginActivity.java
+++ boletafugaz/app/src/main/java/com/example/boletafugaz/LoginActivity.java
@@ -2,13 +2,28 @@ package com.example.boletafugaz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btn_ir_crearCuenta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_ir_crearCuenta=findViewById(R.id.btn_ir_crearCuenta);
+
+        btn_ir_crearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
+                finish();
+            }
+        });
     }
 }
