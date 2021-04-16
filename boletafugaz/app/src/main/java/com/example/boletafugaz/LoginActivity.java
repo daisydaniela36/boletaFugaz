76a6257e6c72c760644955c122bc9d2324e5d6d5
daisydaniela36 <33319268+daisydaniela36@users.noreply.github.com>
2021-04-16 19:08:30 -0400

    se rescataron datos de la bd
    se tomaron los campos correo y contraseña para acceder al perfil tomandolos desde la bd de firebase

--- boletafugaz/app/src/main/java/com/example/boletafugaz/LoginActivity.java
+++ boletafugaz/app/src/main/java/com/example/boletafugaz/LoginActivity.java
@@ -1,22 +1,75 @@
 package com.example.boletafugaz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText edt_VCorreo, edt_VContraseña;
+    Button btnIngresar,  btn_ir_crearCuenta;
+    TextView txt_Rcontraseña;
 
-    Button btn_ir_crearCuenta;
+    private FirebaseAuth firebaseAuth;
+
+
+    private String correo;
+    private String contraseña;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+
         btn_ir_crearCuenta=findViewById(R.id.btn_ir_crearCuenta);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        edt_VCorreo = (EditText) findViewById(R.id.edt_RCorreo);
+        edt_VContraseña = (EditText) findViewById(R.id.edt_RContraseña);
+        txt_Rcontraseña = (TextView) findViewById(R.id.txt_Rcontraseña);
+        btnIngresar = (Button) findViewById(R.id.btn_Rcontraseña);
+        btn_ir_crearCuenta=findViewById(R.id.btn_ir_crearCuenta);
+
+        txt_Rcontraseña.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,ResetPasswordActivity.class));
+                finish();
+            }
+        });
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                correo = edt_VCorreo.getText().toString();
+                contraseña = edt_VContraseña.getText().toString();
+
+                if(!correo.isEmpty() && !contraseña.isEmpty()){
+                    loginUsuario();
+
+                }else{
+                    Toast.makeText(LoginActivity.this, "Complete los campos", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
         btn_ir_crearCuenta.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +78,20 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void loginUsuario() {
+        firebaseAuth.signInWithEmailAndPassword(correo, contraseña).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(LoginActivity.this,PerfilActivity.class));
+                    finish();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Correo o Contraseña incorrectos",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
