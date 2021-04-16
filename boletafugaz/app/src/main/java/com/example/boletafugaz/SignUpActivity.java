d9d158b07b8bafa1e480b56a32e4c866daa39377
daisydaniela36 <33319268+daisydaniela36@users.noreply.github.com>
2021-04-16 18:24:53 -0400

    conexion boton signup
    se conecto el boton iniciar sesion con la actividad login para que se redirija hay

--- boletafugaz/app/src/main/java/com/example/boletafugaz/SignUpActivity.java
+++ boletafugaz/app/src/main/java/com/example/boletafugaz/SignUpActivity.java
@@ -2,13 +2,27 @@ package com.example.boletafugaz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    Button btnIngresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        btnIngresar = (Button) findViewById(R.id.btn_Vingresar);
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
