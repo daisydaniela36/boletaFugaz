603e06361663be8b9e9d7a3b77afb3c51034e5f4
daisydaniela36 <33319268+daisydaniela36@users.noreply.github.com>
2021-06-10 22:58:32 -0400

    calculadora
    se añadio css de una calculadora al inicio

--- boletafugaz/app/src/main/java/com/example/boletafugaz/ProfileActivity.java
+++ boletafugaz/app/src/main/java/com/example/boletafugaz/ProfileActivity.java
@@ -1,13 +1,23 @@
 package com.example.boletafugaz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,32 +29,51 @@ import androidx.appcompat.widget.Toolbar;
 public class ProfileActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference mDataBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        firebaseAuth = FirebaseAuth.getInstance();
+        mDataBase = FirebaseDatabase.getInstance().getReference();
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_inicio, R.id.nav_factura, R.id.nav_cuenta)
-                .setDrawerLayout(drawer)
-                .build();
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_inicio, R.id.nav_factura, R.id.nav_cuenta).setDrawerLayout(drawer).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        getUser();
+    }
+
+    private void getUser() {
+        String id = firebaseAuth.getCurrentUser().getUid();
+        mDataBase.child("usuario").child(id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    String nombre = snapshot.child("nombre").getValue().toString();
+                    String correo = snapshot.child("correo").getValue().toString();
+
+                    View header = ((NavigationView)findViewById(R.id.nav_view)).getHeaderView(0);
+                    ((TextView) header.findViewById(R.id.txt_correo)).setText(correo);
+                    ((TextView) header.findViewById(R.id.txt_nombre)).setText(nombre);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
@@ -52,6 +81,20 @@ public class ProfileActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.profile, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        int id = item.getItemId();
+
+        if(id == R.id.action_cerrar_sesion){
+            firebaseAuth.signOut();
+            startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+            finish();
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
--- boletafugaz/app/src/main/java/com/example/boletafugaz/ui/InicioFragment.java
+++ boletafugaz/app/src/main/java/com/example/boletafugaz/ui/InicioFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,11 +14,36 @@ import androidx.fragment.app.Fragment;
 import com.example.boletafugaz.R;
 
 public class InicioFragment extends Fragment {
+    private Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btn00, btnX, btnMas, btnImprimir, btnIgualC, btnACC;
+    private TextView lblTotal, txtPrecio, lblCantidadItemC;
+    private ImageButton btnClear;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_inicio, container, false);
 
+        btn1 = root.findViewById(R.id.btnN1);
+        btn2 = root.findViewById(R.id.btnN2);
+        btn3 = root.findViewById(R.id.btnN3);
+        btn4 = root.findViewById(R.id.btnN4);
+        btn5 = root.findViewById(R.id.btnN5);
+        btn6 = root.findViewById(R.id.btnN6);
+        btn7 = root.findViewById(R.id.btnN7);
+        btn8 = root.findViewById(R.id.btnN8);
+        btn9 = root.findViewById(R.id.btnN9);
+        btn0 = root.findViewById(R.id.btnN0);
+        btn00 = root.findViewById(R.id.btnNdoble0);
+        btnX = root.findViewById(R.id.btnMultiplicar);
+        btnMas = root.findViewById(R.id.btnAgregar);
+        btnClear = root.findViewById(R.id.btnClear);
+        btnImprimir = root.findViewById(R.id.btnImprimir);
+        btnIgualC = root.findViewById(R.id.btnIgual);
+        btnACC = root.findViewById(R.id.btnAC);
+        txtPrecio = root.findViewById(R.id.txtPrecio);
+        lblTotal = root.findViewById(R.id.lblMostrarTotal);
+        lblCantidadItemC = root.findViewById(R.id.lblCantProductos);
+
         return root;
     }
+
 }
